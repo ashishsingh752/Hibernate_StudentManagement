f1b9d81bbe3451f97156e047865a15af0a2c8055
Ashish <theashish752@gmail.com>
2025-05-18 15:16:24 +0530

    Refactor Student entity and enhance App class with Hibernate persistence logic

--- src/main/java/com/lcwd/hibernate/App.java
+++ src/main/java/com/lcwd/hibernate/App.java
@@ -1,16 +1,48 @@
 package com.lcwd.hibernate;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import com.lcwd.hibernate.entities.Student;
 import com.lcwd.hibernate.util.SessionProvider;
+
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+    	
+        System.out.println( "Hello World! I'm learning Hibernate" );        
+        
+        Student student = new Student();
+        
+        student.setName("Ashish Singh");
+        student.setCollegeName("NIT Rourkela");
+        student.setFather("Purushottam Singh");
+        student.setPhone("9125993492");
+        student.setActive(false);
+        student.setAbout("Electrical Engineer");
         
         SessionFactory sessionFactory = SessionProvider.getSessionFactory();
-        System.out.println(sessionFactory);
+//        System.out.println(sessionFactory);
+        Session session = sessionFactory.openSession();
+        
+        Transaction transaction = null;
+        
+        try {
+			transaction = session.beginTransaction();
+			session.persist(student);
+			System.out.println("Student Saved Successfully");
+			transaction.commit();
+		} catch (Exception e) {
+			if(transaction!=null) {
+				transaction.rollback();
+			}
+			System.out.println("error:  " + e.getMessage());
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+        
     }
 }
--- src/main/java/com/lcwd/hibernate/entities/Student.java
+++ src/main/java/com/lcwd/hibernate/entities/Student.java
@@ -14,26 +14,98 @@ import jakarta.persistence.Table;
 public class Student {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private String id;
-	
-	@Column(name = "Student Name", length = 100, unique = true)
+	private Long id;
+
+	@Column(name = "student_name", length = 100)
 	private String name;
-	
-	@Column(name = "College Name", length = 200, nullable = true)
+
+	@Column(name = "college_name", length = 200)
 	private String collegeName;
-	
-	@Column(name = "Father Name", length = 100)
+
+	@Column(name = "father_name", length = 100)
 	private String father;
-	
-	@Column(name = "Phone No.", length = 12, unique = true)
+
+	@Column(name = "phone_no", length = 12, unique = true)
 	private String phone;
-	
-	@Column(name = "Status")
-	private boolean active=true;
-	
+
+	@Column(name = "status")
+	private boolean active = true;
+
 	@Lob
 	private String about;
+
 	
+	public Student() {};
+
+	public Student(Long id, String name, String collegeName, String father, String phone, boolean active,
+			String about) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.collegeName = collegeName;
+		this.father = father;
+		this.phone = phone;
+		this.active = active;
+		this.about = about;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCollegeName() {
+		return collegeName;
+	}
+
+	public void setCollegeName(String collegeName) {
+		this.collegeName = collegeName;
+	}
+
+	public String getFather() {
+		return father;
+	}
+
+	public void setFather(String father) {
+		this.father = father;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public String getAbout() {
+		return about;
+	}
+
+	public void setAbout(String about) {
+		this.about = about;
+	}
+	
+		
 	 
 	
 }
